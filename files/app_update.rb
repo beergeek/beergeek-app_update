8f768cc74dbfaa69f803037b243edaf8fd8293d2
beergeek <brett.gray@puppetlabs.com>
2015-03-26 08:43:46 +1300

    new updates

--- files/app_update.rb
+++ files/app_update.rb
@@ -21,49 +21,46 @@ module MCollective
       end
 
       def disable_puppet
-	begin
-	  Log.info('Disabling Puppet')
+        begin
+          Log.info('Disabling Puppet')
           agent_msg = @puppet_agent.disable!
         rescue => e
-          reply.fail(reply[:status] = "Could not disable Puppet: %s" % e.to_s)
+          raise "Could not disable Puppet: #{e.to_s}"
         end
-
-        return @puppet_agent.status[:enabled]
       end
 
       def enable_puppet
-	begin
-	  Log.info('Enabling Puppet')
+        begin
+          Log.info('Enabling Puppet')
           agent_msg = @puppet_agent.enable!
         rescue => e
-          reply.fail(reply[:status] = "Could not enable Puppet: %s" % e.to_s)
+          reply.fail(reply[:status] = e.to_s)
         end
-
-        return @puppet_agent.status[:enabled]
       end
 
       def service_manage(svc_name, cmd)
         begin
           x = ::Puppet::Resource.new('service', svc_name, :parameters => {'ensure' => cmd})
-	        result = ::Puppet::Resource.indirection.save(x)
-	        Log.info("#{cmd} the service of #{svc_name}")
+          result = ::Puppet::Resource.indirection.save(x)
+          Log.info("#{cmd} the service of #{svc_name}")
         rescue => e
-          Log.debug("Issue with performing #{cmd} on #{svc_name} service")
-	        reply.fail(reply[:status] = "Could not manage service #{svc_name}: %s" % e.to_s)
-          return -1
+          raise "Could not manage service #{svc_name}: #{e.to_s}"
         end
       end
 
       action 'deploy_app_update' do
         begin
-          disable = disable_puppet
-          kill_svc = service_manage(request[:service], 'stopped')
+          disable_puppet
+          service_manage(request[:service], 'stopped')
           Log.info('We would do our code base copy here')
-          start_svc = service_manage(request[:service], 'running')
-          enable = enable_puppet
+          service_manage(request[:service], 'running')
+          enable_puppet
           reply[:exitcode] = 0
           reply[:out] ='done'
         rescue => e
+          Log.err(e.to_s)
+          reply.fail(reply[:status] = e.to_s)
+        ensure
           enable = enable_puppet
         end
       end
